dacaa87123dccd73df250fb426ebcf329fd4d114
Elian David Betancourt Negrete <ebetancourt99@correo.unicordoba.edu.co>
2021-09-10 14:25:23 -0500

    Add Complete
    Chat funcionando en celular y computador

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) => {  
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-  });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+});
 
-http.listen(3000, () => {
+server.listen(3000, () => {
   console.log('listening on *:3000');
 });
